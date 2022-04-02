b8489af3be41d40169b5941af3be0d2418ba2382
GRZEGORZ SZWED <grzesiek.szwed@gmail.com>
2022-04-02 21:26:27 +0200

    upload to sqlite

---
+++ training/SQLite/config.py
@@ -0,0 +1,40 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        return conn
+    #finally:
+    #    if conn:
+    #       return conn.close() 
+    #      
+
+
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+        print('Sql executed')
+    except Error as e:
+        print(e)
+
+
+
+
+database = r"C:\SQLite\db\pythonsqlite.db"
+
+#create_connection(database)
